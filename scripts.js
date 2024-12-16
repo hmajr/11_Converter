1fe09cbf8521aeb8f437e35a067de4e2aeb70ae3
Helio M. Andrade <hmajr@users.noreply.github.com>
2024-12-16 00:29:31 -0300

    Create convert currency function

--- scripts.js
+++ scripts.js
@@ -15,4 +15,32 @@ amount.addEventListener("input", () => {
 form.onsubmit = (event) => {
   event.preventDefault()
 
+  switch (currency.value) {
+    case "USD":
+      convertCurrency(amount.value, USD, "US$")
+      break;
+  
+    case "EUR":
+      convertCurrency(amount.value, EUR, "€")
+      break;
+  
+    case "GBP":
+      convertCurrency(amount.value, GBP, "£")
+      break;
+  }
+}
+
+function convertCurrency(amount, price, symbol) {
+  try {
+    footer.classList.add("show-result")
+
+    description.textContent = `${symbol} 1 = R$ ${formatCurrencyBRL(price)}`
+
+    let total = formatCurrencyBRL(amount * price)
+    result.textContent = `${total} Reais`
+  } catch (error) {
+    console.log(error)
+    footer.classList.remove("show-result")
+    console.alert("Não foi possível converter. Tente mais tarde")
+  }
 }
